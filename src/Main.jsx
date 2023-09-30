563c0087d602e38cc9e0f6a0522061f33ea62879
OlenaKravchenko2022 <107769679+OlenaKravchenko2022@users.noreply.github.com>
2023-09-30 12:36:55 +0200

    todo

---
+++ src/Main.jsx
@@ -0,0 +1,73 @@
+import { useState, useEffect } from "react";
+import List from "./components/List";
+import { v4 as uuidv4 } from "uuid";
+
+function Main() {
+  const [tasks, setTasks] = useState(() => {
+    const storedTodos = localStorage.getItem("tasks");
+    if (!storedTodos) {
+      return [];
+    } else {
+      return JSON.parse(storedTodos);
+    }
+  });
+
+  const [taskTitle, setTasktitle] = useState("");
+
+  useEffect(() => {
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+  }, [tasks]);
+
+  const addTask = (e) => {
+    const storedTodos = JSON.parse(localStorage.getItem("tasks"));
+    if (e.key === "Enter" && e.target.value !== "") {
+      setTasks([
+        ...storedTodos,
+        {
+          id: uuidv4(),
+          title: taskTitle,
+          status: false,
+        },
+      ]);
+      setTasktitle("");
+    }
+  };
+
+  const date = new Date();
+  const monthNames = [
+    "January",
+    "Febrary",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+  const month = monthNames[date.getMonth()];
+  const day = date.getDate();
+  const year = date.getFullYear();
+
+  return (
+    <div className="container">
+      <h1>Note your tasks</h1>
+      <h2>Write it here</h2>
+      <span>{month + " " + day + "," + " " + year + " "}</span>
+      <div className="input-field">
+        <input
+          type="text"
+          value={taskTitle}
+          onChange={(event) => setTasktitle(event.target.value)}
+          onKeyDown={addTask}
+        />
+        <label>Task name</label>
+      </div>
+      <List tasks={tasks} />
+    </div>
+  );
+}
+export default Main;
---
+++ src/components/Item.jsx
@@ -0,0 +1,33 @@
+import { useState } from "react";
+
+export default function Item({ title, id, status }) {
+  const classes = ["todo"];
+  const [checked, setChecked] = useState(status);
+
+  if (checked) {
+    classes.push("status");
+  }
+
+  const updateStatus = () => {
+    setChecked(!checked);
+    const storedTodos = JSON.parse(localStorage.getItem("tasks"));
+    storedTodos.map((el) => {
+      if (el.id === id) {
+        el.status = !checked;
+      }
+      return true;
+    });
+
+    localStorage.setItem("tasks", JSON.stringify(storedTodos));
+  };
+
+  return (
+    <li className={classes.join(" ")}>
+      <label>
+        <input type="checkbox" checked={checked} onChange={updateStatus} />
+        <span>{title}</span>
+        <i className="material-icons red-text">X</i>
+      </label>
+    </li>
+  );
+}
